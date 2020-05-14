9e7a437590f22f344212d0033c249f8bf4b3fae8
Lincoln Stein <lincolntstein@gmail.com>
2020-05-14 15:55:51 -0500

    WIP MRTK hand interaction demo

---
+++ Assets/Exit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Assets/Exit")]
+public class Exit : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+    }
+}
